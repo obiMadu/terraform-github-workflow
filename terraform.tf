4f8f90e5229a2e0ec0cfdb7c7da81dfe146c9155
Obi.M <mail@obimadu.pro>
2024-02-05 19:05:24 +0100

    Initial commit

---
+++ terraform.tf
@@ -0,0 +1,12 @@
+terraform {
+  required_providers {
+    digitalocean = {
+      source = "digitalocean/digitalocean"
+      version = "2.34.1"
+    }
+    namecheap = {
+      source = "namecheap/namecheap"
+      version = "2.1.1"
+    }
+  }
+}
