68e844d363971a8069d42ad7048e6383376bad24
Obi.M <mail@obimadu.pro>
2024-02-05 19:06:10 +0100

    create variables.tf

---
+++ variables.tf
@@ -0,0 +1,15 @@
+variable "do_token" {
+  type = string
+}
+
+variable "namecheap_user" {
+  type = string
+}
+
+variable "namecheap_api_user" {
+  type = string
+}
+
+variable "namecheap_api_key" {
+  type = string
+}
