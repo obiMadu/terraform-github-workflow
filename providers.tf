d69e5057f13495de3d6f12bd1b4ae7f0f8b6d493
Obi.M <mail@obimadu.pro>
2024-02-12 15:18:01 +0100

    use cloudflare, not namecheap

--- providers.tf
+++ providers.tf
@@ -3,9 +3,6 @@ provider "digitalocean" {
   token = var.do_token
 }
 
-# Namecheap API credentials
-provider "namecheap" {
-  user_name = var.namecheap_user
-  api_user = var.namecheap_api_user
-  api_key = var.namecheap_api_key
+provider "cloudflare" {
+  api_token = var.cloudflare_api_token
 }
