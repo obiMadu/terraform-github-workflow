4b146b95f4b7f9914376f2b86c546afcaa63bfb2
Obi.M <mail@obimadu.pro>
2024-02-12 15:18:15 +0100

    use cloudflare, not namecheap

--- terraform.tf
+++ terraform.tf
@@ -4,9 +4,9 @@ terraform {
       source = "digitalocean/digitalocean"
       version = "2.34.1"
     }
-    namecheap = {
-      source = "namecheap/namecheap"
-      version = "2.1.1"
+    cloudflare = {
+      source = "cloudflare/cloudflare"
+      version = "4.24.0"
     }
   }
 }
